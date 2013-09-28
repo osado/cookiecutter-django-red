605a6aed959c5a1e9d00f489529e5ff10df58af0
Nikolay Udalov <nikolaj.udalov@gmail.com>
2013-09-28 23:16:10 +0400

    misc fixes

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'{{ project_name }}'
+project = u'{{ cookiecutter.project_name }}'
 copyright = u'2013, ChangeMyName'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = '{{ project_name }}doc'
+htmlhelp_basename = '{{ cookiecutter.project_name }}doc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', '{{ project_name }}.tex', u'{{ project_name }} Documentation',
+  ('index', '{{ cookiecutter.project_name }}.tex', u'{{ cookiecutter.project_name }} Documentation',
    u'ChangeToMyName', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', '{{ project_name }}', u'{{ project_name }} Documentation',
+    ('index', '{{ cookiecutter.project_name }}', u'{{ cookiecutter.project_name }} Documentation',
      [u'ChangeToMyName'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', '{{ project_name }}', u'{{ project_name }} Documentation',
-   u'ChangeToMyName', '{{ project_name }}', 'One line description of project.',
+  ('index', '{{ cookiecutter.project_name }}', u'{{ cookiecutter.project_name }} Documentation',
+   u'ChangeToMyName', '{{ cookiecutter.project_name }}', 'One line description of project.',
    'Miscellaneous'),
 ]
 
